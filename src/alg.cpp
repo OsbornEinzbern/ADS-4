fe12454775229c2458f0d9f5fbbfdcf2f0ae9656
Vio5 <160281229+OsbornEinzbern@users.noreply.github.com>
2024-03-25 23:04:19 +0300

    Upd.exp v1.8.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,27 +17,15 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-int i, j, k, rb;
-rb = len - 1;
-for (i = 0; i < len / 2; i++) {
-  if (arr[i] >= 50) {
-    continue;
-  }
-  for (j = rb; j > i; j--) {
-    if (arr[j] >= 50) {
-      rb--;
-      continue;
-    }
-    if (arr[j] + arr[i] == value) {
-      k++;
-    }
+int counter = 0;
+int max = len - 1;
+while (arr[max] > value) max--;
+for (int i = 0; i <= max; i++) {
+  for (int j = i + 1; j <= max; j++) {
+    if (arr[i] + arr[j] == value) counter++;
   }
 }
-if (k > 0) {
-  return k;
-} else {
-  return 0;
-}
+return counter;
 }
 
 int countPairs3(int *arr, int len, int value) {
